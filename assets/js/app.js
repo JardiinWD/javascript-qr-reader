bf9c5cce6383d586300201870a86cc1f80c71e2a
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-21 14:32:38 +0200

    Implementato evento sulla cloud per import image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-/* alert("Ciao") */
+const wrapper = document.querySelector(".wrapper") // Pesco il mio div principale, ovvero il wrapper
+form = document.querySelector("form") // Pesco il mio form, la zona del mio QR Code
+fileInput = form.querySelector("input") // Con questo pesco l'input type file presente nel mio form
+
+/* 
+Aggiunto un evento al mio form, che replica praticamente 
+l'input type. Al click sull'immagine nuvoletta riesco a importare un'immagine. 
+*/
+form.addEventListener("click", () => fileInput.click())
+
