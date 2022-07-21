5020b9a56839c93979731e6933e440628d490e53
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-21 16:29:21 +0200

    Aggiunto possibilità di ricavare il QR da utente

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ infoText = form.querySelector("p"); // Questa è la descrizione del QR Code
  * da questa funzione ne scaturisce un'array di questo QR Code letto e immesso dall'utente
  * @param {object} formData 
  */
-function fetchRequest(formData) {
+function fetchRequest(formData, file) {
     /* Appendo a infoText la preparazione al QR Code */
     infoText.innerHTML = "Scanning QR Code..."
     /* nel fetch faccio la richiesta al QR Server API */
@@ -21,8 +21,10 @@ function fetchRequest(formData) {
         .then(res => res.json()).then(result => {
             result = result[0].symbol[0].data // Questo è il mio QR Code
             console.log(result); // Verifica del risultato
+            wrapper.querySelector("textarea").innerHTML = result // nella text area metto il testo della mia query
             infoText.innerHTML = "Upload QR Code to Scan"
             console.log(result); /* Verifica in console log del risultato */
+            form.querySelector("img").src = URL.createObjectURL(file) // Al src della mia immagine appendo il QR
             wrapper.classList.add("active") // al mio wrapper gli lascio, tramite classList la classe active
         })
 }
@@ -33,7 +35,7 @@ fileInput.addEventListener("change", e => {
     console.log(file); // verifica in console
     let formData = new FormData(); // Crea un nuovo oggetto Form Data
     formData.append("file", file) // Appendo al mio nuovo oggetto il file caricato da utente
-    fetchRequest(formData) // invoco la mia function con parametro formData
+    fetchRequest(formData, file) // invoco la mia function con parametro formData
 })
 
 /* 
